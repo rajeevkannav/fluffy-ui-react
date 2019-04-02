29ce3665ab24b6e89ed95051af8a3420445bcf7e
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-04-02 13:18:38 +0530

    added some very basic routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
+import About from './components/About';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/about/" component={About} />
+        </Switch>
+    </Router>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
