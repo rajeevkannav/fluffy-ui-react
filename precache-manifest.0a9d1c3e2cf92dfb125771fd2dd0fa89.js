31c7831b9396c6324cc3dca65fb8901a7161edba
Rajeev Sharma <rajeevsharma86@gmail.com>
2019-04-02 13:07:42 +0530

    Updates

---
+++ precache-manifest.0a9d1c3e2cf92dfb125771fd2dd0fa89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/fluffy-ui-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "21151d23810215e8f8df",
+    "url": "/fluffy-ui-react/static/js/runtime~main.8d192b1c.js"
+  },
+  {
+    "revision": "0be933f46e4f97afaae0",
+    "url": "/fluffy-ui-react/static/js/main.7dedde16.chunk.js"
+  },
+  {
+    "revision": "10534ad094e56cc863c6",
+    "url": "/fluffy-ui-react/static/js/2.4b44abd2.chunk.js"
+  },
+  {
+    "revision": "0be933f46e4f97afaae0",
+    "url": "/fluffy-ui-react/static/css/main.eaedcddd.chunk.css"
+  },
+  {
+    "revision": "25d626d3ff1f11e002a3991d681e32de",
+    "url": "/fluffy-ui-react/index.html"
+  }
+];
